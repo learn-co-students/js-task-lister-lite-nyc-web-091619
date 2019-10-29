057a5049e9fbb888f545de430f637eb69ed1245c
VPoshelyuk <aslava198@gmail.com>
2019-10-29 17:54:29 -0400

    done

--- src/index.js
+++ src/index.js
@@ -1,3 +1,16 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  let submit = document.querySelector("input[type=submit]")
+  let ul = document.getElementsByTagName("ul")[0]
+  submit.addEventListener("click", function(e){
+    let input = document.getElementById("new-task-description").value
+    li = document.createElement("li")
+    li.innerText = `${input}`
+    delete_button = document.createElement("button")
+    delete_button.innerText = "X"
+    li.appendChild(delete_button)
+    ul.appendChild(li)
+    e.preventDefault()
+  })
+
+})
