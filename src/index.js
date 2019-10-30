dfa0d9c896800cd787ed72419496c3bc7e39cedc
VPoshelyuk <aslava198@gmail.com>
2019-10-29 21:10:26 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -2,15 +2,29 @@ document.addEventListener("DOMContentLoaded", () => {
   // your code here
   let submit = document.querySelector("input[type=submit]")
   let ul = document.getElementsByTagName("ul")[0]
-  submit.addEventListener("click", function(e){
-    let input = document.getElementById("new-task-description").value
+  
+  let deletePressed = el => {
+    let parent = el.target.parentNode
+    parent.remove()
+    
+  }
+
+  let submitClick = e => {
+    let input = document.getElementById("new-task-description")
     li = document.createElement("li")
-    li.innerText = `${input}`
+    li.innerText = `${input.value}`
     delete_button = document.createElement("button")
     delete_button.innerText = "X"
     li.appendChild(delete_button)
     ul.appendChild(li)
+    
+    delete_button.addEventListener("click", deletePressed)
+    input.value = ''
     e.preventDefault()
-  })
+  }
+  
+  submit.addEventListener("click", submitClick)
+  
+
 
 })
