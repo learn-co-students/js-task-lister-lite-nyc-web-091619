0550e460132ec04bc2937aec8a9a9e5707b29cba
Altaf Quadri <altafmquadri@gmail.com>
2019-11-05 11:22:43 -0500

    added new implementation

--- src/index.js
+++ src/index.js
@@ -1,15 +1,68 @@
 document.addEventListener("DOMContentLoaded", () => {
-  let submit = document.querySelector("input[type=submit]")
-  console.log(submit)
-  let task = document.getElementById("tasks")
-  submit.addEventListener('click', function(e) {
-    console.log(e)
-  e.preventDefault()
-  let li = document.createElement("li")
-  task.appendChild(li)
-  let text = document.getElementById("new-task-description").value
-  li.innerText = text
-  
+    
+const ul = document.getElementById("tasks")
+const form = document.getElementById("create-task-form")
+
+form.addEventListener('submit', function(e){
+    e.preventDefault()
+    let li = document.createElement("li")
+    li.innerText = e.target[0].value
+    ul.appendChild(li)
+    form.reset()
+
+    //delete li functionality
+    let button = document.createElement("button")
+    button.innerText = "x"
+    li.appendChild(button)
+    button.addEventListener('click', function(e){
+        e.target.parentNode.remove()
+    })
+
+    //priority selection
+    let select = document.createElement("select")
+    select.className = "selection"
+    let red = document.createElement("option")
+    red.innerText = "High"
+    let yellow = document.createElement("option")
+    yellow.innerText = "Medium"
+    let green = document.createElement("option")
+    green.innerText = "Low"
+    let normal = document.createElement("option")
+    normal.innerText = "Default"
+    normal.selected = true
+
+    select.appendChild(red)
+    select.appendChild(yellow)
+    select.appendChild(green)
+    select.appendChild(normal)
+
+    li.appendChild(select)
+
+    select.addEventListener('change', function (e){
+        if (e.target.value === "High") {
+            e.target.parentNode.style.color = "red"
+        } else if (e.target.value === "Medium") {
+            e.target.parentNode.style.color = "yellow"
+        } else if (e.target.value === "Low") {
+            e.target.parentNode.style.color = "green"
+        } else if (e.target.value === "Default") {
+            e.target.parentNode.style.color = "black"
+        }
+    })
+
+
+
+})
+
+    
+    
 });
 
-});
+
+
+
+
+
+
+
+
